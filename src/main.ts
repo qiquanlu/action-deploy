e43559e660d53221fd4f5aecc1600f8235351ba9
Qiquan Lu <kejeon@gmail.com>
2024-01-06 21:13:13 -0500

    lint

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { wait } from './wait'
 export async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    core.debug(`github:\n${JSON.stringify(github.context,null,4)}`)
+    core.debug(`github:\n${JSON.stringify(github.context, null, 4)}`)
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Waiting ${ms} milliseconds ...`)
 
